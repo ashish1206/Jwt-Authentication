a113e95fea2cf2fdf21b403a7976a8938a622dcd
ashish1206 <ashishchauhan1206@gmail.com>
2020-10-10 16:40:10 +0530

    refactoring

--- src/main/java/com/project/auth/controller/AuthController.java
+++ src/main/java/com/project/auth/controller/AuthController.java
@@ -4,14 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import com.project.auth.model.AuthRequest;
 import com.project.auth.model.AuthResponse;
 import com.project.auth.service.AuthService;
